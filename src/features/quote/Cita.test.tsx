5d13ecb2fdcd69d9301d244002d6cbac30a713c6
MarlenedosSantos <marlene.dossantoss97@gmail.com>
2023-07-10 17:02:55 -0300

    Terminado

--- src/features/quote/Cita.test.tsx
+++ src/features/quote/Cita.test.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from "../../test-utils";
 import { server } from "../../mocks/server";
-import Cita from "./Cita";
 import userEvent from "@testing-library/user-event";
 import { waitFor } from "@testing-library/react";
+import Cita from "./Cita";
+
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
@@ -14,13 +15,6 @@ describe("Testeando componente Cita", () => {
     expect(screen.getByText("No se encontro ninguna cita")).toBeInTheDocument();
   });
 
-  test("Mostrar el placeholder: Ingresa el nombre del autor, en el input", () => {
-    render(<Cita />);
-    expect(
-      screen.getByPlaceholderText("Ingresa el nombre del autor")
-    ).toBeInTheDocument();
-  });
-
   test("Mostrar la leyenda: CARGANDO...", async () => {
     render(<Cita />);
     const button = screen.getByText("Obtener cita aleatoria");
@@ -28,29 +22,36 @@ describe("Testeando componente Cita", () => {
     expect(await screen.findByText("CARGANDO...")).toBeInTheDocument();
   });
 
-  test("Mostrar una cita aleatoria", async () => {
+  test("Mostrar el placeholder: Ingresa el nombre del autor, en el input", () => {
     render(<Cita />);
-    const button = screen.getByText("Obtener cita aleatoria");
-    userEvent.click(button);
-    await waitFor(() => expect(screen.queryByText("Duffman")));
+    expect(
+      screen.getByPlaceholderText("Ingresa el nombre del autor")
+    ).toBeInTheDocument();
   });
 
-  test("Mostrar una cita de Milhouse Van Houten", async () => {
-    render(<Cita />);
-    const input = screen.getByPlaceholderText("Ingresa el nombre del autor");
-    await userEvent.type(input, "Milhouse");
-    const button = screen.getByText("Obtener Cita");
-    await userEvent.click(button);
-    await waitFor(() => expect(screen.queryByText("Milhouse Van Houten")));
-  });
-
-  test("Mostrar mensaje 'Por favor ingrese un nombre válido' al ingresr números", async () => {
+  test("Mostrar mensaje 'Por favor ingrese un nombre válido' al ingresar números", async () => {
     render(<Cita />);
     const input = screen.getByPlaceholderText('Ingresa el nombre del autor');
     await userEvent.type(input, '123456');
     const button = screen.getByText('Obtener Cita');
     await userEvent.click(button);
     expect(await screen.findByText('Por favor ingrese un nombre válido')).toBeInTheDocument();
+  });
+
+  test("Mostrar una cita aleatoria", async () => {
+    render(<Cita />);
+    const button = screen.getByText("Obtener cita aleatoria");
+    userEvent.click(button);
+    await waitFor(() => expect(screen.queryByText("Lisa Simpson")));
+  });
+
+  test("Mostrar una cita de Marge Simpson", async () => {
+    render(<Cita />);
+    const input = screen.getByPlaceholderText("Ingresa el nombre del autor");
+    await userEvent.type(input, "Marge");
+    const button = screen.getByText("Obtener Cita");
+    await userEvent.click(button);
+    await waitFor(() => expect(screen.queryByText("Marge Simpson")));
   });
 
 });
